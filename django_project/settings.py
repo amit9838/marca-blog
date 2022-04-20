77c767d35ba5d3ef9adaf51b2922d365d3bbfdd9
Amit Chaudhary <amitchaudhary0539@gmail.com>
2022-04-21 00:42:57 +0530

    initial commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
     "https://marca-blog.herokuapp.com/",
     "http://marca-blog.herokuapp.com/",
     "marca-blog.herokuapp.com/",
+    'marca-blog.herokuapp.com',
     "127.0.0.1",
 ]
 
